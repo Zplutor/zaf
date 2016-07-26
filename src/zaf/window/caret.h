4027dc62ea6a1b6ae652a86cd98960a38bf8b057
Zplutor <zplutor@qq.com>
2016-07-26 22:56:36 +0800

    Add documentation to Caret.

--- src/zaf/window/caret.h
+++ src/zaf/window/caret.h
@@ -9,26 +9,69 @@ class Canvas;
 class Timer;
 class Window;
 
+/**
+ Represents an input caret in a window.
+
+ There is no need to create instance of Caret by yourself. Call Window::GetCaret method
+ to get the unique caret in the window.
+ */
 class Caret {
 public:
+    /**
+     Construct the instance.
+     */
 	Caret();
+
+    /**
+     Destruct the instance.
+     */
 	~Caret();
 
+    /**
+     Get the caret's rect.
+
+     The default rect is empty, you should use SetRect method to set a 
+     appropriate rect.
+     */
 	const Rect& GetRect() const {
 		return rect_;
 	}
 
+    /**
+     Set the caret's rect.
+     */
 	void SetRect(const Rect& rect);
 
+    /**
+     Set the caret's position.
+
+     This is a shortcut of SetRect.
+     */
 	void SetPosition(const Point& position) {
 		SetRect(Rect(position, GetRect().size));
 	}
 
+    /**
+     Set the caret's size.
+
+     This is a shortcut of SetRect.
+     */
 	void SetSize(const Size& size) {
 		SetRect(Rect(GetRect().position, size));
 	}
 
+    /**
+     Show the caret.
+
+     The caret starts blinking.
+     */
 	void Show();
+
+    /**
+     Hide the caret.
+
+     The caret stops blinking.
+     */
 	void Hide();
 
 private:
