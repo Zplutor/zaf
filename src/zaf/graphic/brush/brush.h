5850239e9a7a255bbdb37eeb87611f6867e09647
Zplutor <zplutor@qq.com>
2016-08-04 00:19:19 +0800

    Add documentation.

--- src/zaf/graphic/brush/brush.h
+++ src/zaf/graphic/brush/brush.h
@@ -4,6 +4,11 @@
 
 namespace zaf {
 
+/**
+ Represents a brush.
+
+ This a base class of all concrete brush.
+ */
 class Brush {
 public:
 	virtual ~Brush() {
@@ -13,24 +18,38 @@ public:
 		}
 	}
 
+    /**
+     Get opacity.
+     */
 	float GetOpacity() const {
 		return handle_->GetOpacity();
 	}
 
+    /**
+     Set opacity.
+     */
 	void SetOpacity(float opacity) {
 		handle_->SetOpacity(opacity);
 	}
 
+    /**
+     Get brush's handle.
+     */
 	ID2D1Brush* GetHandle() const {
 		return handle_;
 	}
 
-protected:
-	explicit Brush(ID2D1Brush* handle) : handle_(handle) { }
+    Brush(const Brush&) = delete;
+    Brush& operator=(const Brush&) = delete;
 
-private:
-	Brush(const Brush&) = delete;
-	Brush& operator=(const Brush&) = delete;
+protected:
+    /**
+     Construct the instance with specified handle.
+
+     The brush instance takes over the lifetime of handle. It would
+     release the handle when destroyed.
+     */
+	explicit Brush(ID2D1Brush* handle) : handle_(handle) { }
 
 private:
 	ID2D1Brush* handle_;
--- src/zaf/graphic/brush/solid_color_brush.h
+++ src/zaf/graphic/brush/solid_color_brush.h
@@ -5,16 +5,32 @@
 
 namespace zaf {
 
+/**
+ Represents a solid color brush.
+
+ You should create instance via Renderer::CreateSolidColorBrush.
+ */
 class SolidColorBrush : public Brush {
 public:
+    /**
+     Construct the instance with specified handle.
+
+     See also Brush::Brush.
+     */
 	explicit SolidColorBrush(ID2D1SolidColorBrush* handle) : 
 		Brush(handle),
 		handle_(handle) { }
 
+    /**
+     Get color of brush.
+     */
 	const Color GetColor() const {
 		return Color::FromD2D1COLORF(handle_->GetColor());
 	}
 
+    /**
+     Set color of brush.
+     */
 	void SetColor(const Color& color) {
 		handle_->SetColor(color.ToD2D1COLORF());
 	}
