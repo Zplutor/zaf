89ae8399b47acd094bdef76cb1185e839ed23757
Zplutor <zplutor@qq.com>
2017-01-13 14:05:12 +0800

    Add ListBox explore panel to control explorer example.

--- example/control_explorer/src/main_window.cpp
+++ example/control_explorer/src/main_window.cpp
@@ -6,6 +6,7 @@
 #include "control/button/button_explore_manager.h"
 #include "control/check_box/check_box_explore_manager.h"
 #include "control/label/label_explore_manager.h"
+#include "control/list_box/list_box_explore_manager.h"
 #include "control/radio_button/radio_button_explore_manager.h"
 #include "control/text_box/text_box_explore_manager.h"
 
@@ -16,6 +17,7 @@ static const struct {
     L"Button", []() { return std::make_shared<ButtonExploreManager>(); },
     L"CheckBox", []() { return std::make_shared<CheckBoxExploreManager>(); },
     L"Label", []() { return std::make_shared<LabelExploreManager>(); },
+    L"ListBox", []() { return std::make_shared<ListBoxExploreManager>(); },
     L"RadioButton", []() { return std::make_shared<RadioButtonExploreManager>(); },
     L"TextBox", []() { return std::make_shared<TextBoxExploreManager>(); },
 };
---
+++ src/zaf/property/list_box_property_tag.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <zaf/control/list_box.h>
+#include <zaf/property/property_tag.h>
+
+namespace zaf {
+
+ZAF_DEFINE_PROPERTY_TAG(itemHeight, float, ListBox, SetItemHeight);
+
+}
