16c9d259b95e2a8f156953ab25bb1739a97681f0
Zplutor <zplutor@qq.com>
2017-03-03 14:17:58 +0800

    Remove test project and add playground project.

---
+++ playground/main.cpp
@@ -0,0 +1,36 @@
+#include <Windows.h>
+#include <zaf/application.h>
+#include <zaf/base/error.h>
+#include <zaf/creation.h>
+#include <zaf/window/window.h>
+
+void BeginRun(zaf::Application&);
+
+int WINAPI WinMain(
+    HINSTANCE /* hInstance */,
+    HINSTANCE /* hPrevInstance */,
+    LPSTR /* lpCmdLine */,
+    int /* nCmdShow */
+) {
+
+    auto& application = zaf::Application::GetInstance();
+    application.GetBeginRunEvent().AddListener(BeginRun);
+
+    std::error_code error_code;
+    application.Initialize(error_code);
+
+    if (zaf::IsSucceeded(error_code)) {
+        application.Run();
+    }
+}
+
+
+void BeginRun(zaf::Application& application) {
+
+    auto window = zaf::Create<zaf::Window>();
+    window->SetTitle(L"zaf playground");
+    window->SetRect(zaf::Rect(200, 200, 500, 500));
+
+    window->Show();
+    application.SetMainWindow(window);
+}
