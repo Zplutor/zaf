3e39a992237e5b498c7770925d3a416769f2a359
zephyryu <zephyryu@tencent.com>
2018-11-21 17:03:31 +0800

    Use std::underlying_type_t in flag enum operators.

--- src/zaf/base/flag_enum.h
+++ src/zaf/base/flag_enum.h
@@ -1,24 +1,38 @@
 #pragma once
 
-#define ZAF_ENABLE_FLAG_ENUM(Enum)                                                              \
-inline Enum operator~(Enum value) {                                                             \
-	return static_cast<Enum>(~static_cast<int>(value));                                         \
-}                                                                                               \
-inline Enum operator|(Enum value1, Enum value2) {                                               \
-	return static_cast<Enum>(static_cast<int>(value1) | static_cast<int>(value2));              \
-}                                                                                               \
-inline Enum operator&(Enum value1, Enum value2) {                                               \
-	return static_cast<Enum>(static_cast<int>(value1) & static_cast<int>(value2));              \
-}                                                                                               \
-inline Enum operator^(Enum value1, Enum value2) {                                               \
-	return static_cast<Enum>(static_cast<int>(value1) ^ static_cast<int>(value2));              \
-}                                                                                               \
-inline Enum& operator|=(Enum& value1, Enum value2) {                                            \
-	return reinterpret_cast<Enum&>(reinterpret_cast<int&>(value1) |= static_cast<int>(value2)); \
-}                                                                                               \
-inline Enum& operator&=(Enum& value1, Enum value2) {                                            \
-	return reinterpret_cast<Enum&>(reinterpret_cast<int&>(value1) &= static_cast<int>(value2)); \
-}                                                                                               \
-inline Enum& operator^=(Enum& value1, Enum value2) {                                            \
-	return reinterpret_cast<Enum&>(reinterpret_cast<int&>(value1) ^= static_cast<int>(value2)); \
+#include <type_traits>
+
+#define ZAF_ENABLE_FLAG_ENUM(Enum)                                                  \
+inline Enum operator~(Enum value) {                                                 \
+    return static_cast<Enum>(~static_cast<std::underlying_type_t<Enum>>(value));    \
+}                                                                                   \
+inline Enum operator|(Enum value1, Enum value2) {                                   \
+    return static_cast<Enum>(                                                       \
+        static_cast<std::underlying_type_t<Enum>>(value1) |                         \
+        static_cast<std::underlying_type_t<Enum>>(value2));                         \
+}                                                                                   \
+inline Enum operator&(Enum value1, Enum value2) {                                   \
+    return static_cast<Enum>(                                                       \
+        static_cast<std::underlying_type_t<Enum>>(value1) &                         \
+        static_cast<std::underlying_type_t<Enum>>(value2));                         \
+}                                                                                   \
+inline Enum operator^(Enum value1, Enum value2) {                                   \
+    return static_cast<Enum>(                                                       \
+        static_cast<std::underlying_type_t<Enum>>(value1) ^                         \
+        static_cast<std::underlying_type_t<Enum>>(value2));                         \
+}                                                                                   \
+inline Enum& operator|=(Enum& value1, Enum value2) {                                \
+    return reinterpret_cast<Enum&>(                                                 \
+        reinterpret_cast<std::underlying_type_t<Enum>&>(value1) |=                  \
+        static_cast<std::underlying_type_t<Enum>>(value2));                         \
+}                                                                                   \
+inline Enum& operator&=(Enum& value1, Enum value2) {                                \
+    return reinterpret_cast<Enum&>(                                                 \
+        reinterpret_cast<std::underlying_type_t<Enum>&>(value1) &=                  \
+        static_cast<std::underlying_type_t<Enum>>(value2));                         \
+}                                                                                   \
+inline Enum& operator^=(Enum& value1, Enum value2) {                                \
+    return reinterpret_cast<Enum&>(                                                 \
+        reinterpret_cast<std::underlying_type_t<Enum>&>(value1) ^=                  \
+        static_cast<std::underlying_type_t<Enum>>(value2));                         \
 }
