57405fe180545064f938914c189f51e91b78c903
Zplutor <zplutor@qq.com>
2019-07-21 20:38:45 +0800

    Add StableSort.

--- src/zaf/base/container/utility/sort.h
+++ src/zaf/base/container/utility/sort.h
@@ -1,17 +1,91 @@
 #pragma once
 
 #include <algorithm>
+#include <zaf/base/container/utility/classify.h>
 
 namespace zaf {
+namespace internal {
+
+template<typename C>
+struct GeneralSorter {
+
+    static void Sort(C& container) {
+        std::sort(container.begin(), container.end());
+    }
+
+    template<typename P>
+    static void Sort(C& container, P&& predicate) {
+        std::sort(container.begin(), container.end(), std::forward<P>(predicate));
+    }
+
+    static void StableSort(C& container) {
+        std::stable_sort(container.begin(), container.end());
+    }
+
+    template<typename P>
+    static void StableSort(C& container, P&& predicate) {
+        std::stable_sort(container.begin(), container.end(), std::forward<P>(predicate));
+    }
+};
+
+
+template<typename C>
+struct ListSorter {
+
+    static void Sort(C& container) {
+        container.sort();
+    }
+
+    template<typename P>
+    static void Sort(C& container, P&& predicate) {
+        container.sort(std::forward<P>(predicate));
+    }
+
+    static void StableSort(C& container) {
+        Sort(container);
+    }
+
+    template<typename P>
+    static void StableSort(C& container, P&& predicate) {
+        Sort(container, std::forward<P>(predicate));
+    }
+};
+
+
+template<typename C>
+struct GetSorter {
+
+    static_assert(
+        !IsAssociativeContainer<C>::Value,
+        "zaf::Sort() family functions can not be used with associative containers.");
+
+    using Type = std::conditional_t<IsListContainer<C>::Value, ListSorter<C>, GeneralSorter<C>>;
+};
+
+template<typename C>
+using Sorter = typename GetSorter<C>::Type;
+
+}
 
 template<typename C>
 void Sort(C& container) {
-	std::sort(container.begin(), container.end());
+    internal::Sorter<C>::Sort(container);
 }
 
 template<typename C, typename P>
-void Sort(C& container, P predicate) {
-	std::sort(container.begin(), container.end(), std::move(predicate));
+void Sort(C& container, P&& predicate) {
+    internal::Sorter<C>::Sort(container, std::forward<P>(predicate));
+}
+
+
+template<typename C>
+void StableSort(C& container) {
+    internal::Sorter<C>::StableSort(container);
+}
+
+template<typename C, typename P>
+void StableSort(C& container, P&& predicate) {
+    internal::Sorter<C>::StableSort(container, std::forward<P>(predicate));
 }
 
 
@@ -23,10 +97,26 @@ C MakeSorted(C&& container) {
 }
 
 template<typename C, typename P>
-C MakeSorted(C&& container, P predicate) {
+C MakeSorted(C&& container, P&& predicate) {
 	C new_container{ std::forward<C>(container) };
-	Sort(container, std::move(predicate));
+	Sort(container, std::forward<P>(predicate));
 	return new_container;
 }
+
+
+template<typename C>
+C MakeStableSorted(C&& container) {
+    C new_container{ std::forward<C>(container) };
+    StableSort(new_container);
+    return new_container;
+}
+
+template<typename C, typename P>
+C MakeStableSorted(C&& container, P&& predicate) {
+    C new_container{ std::forward<C>(container) };
+    StableSort(container, std::forward<P>(predicate));
+    return new_container;
+}
+
 
 }
--- unittest/case/base/container/utility/sort_test.cpp
+++ unittest/case/base/container/utility/sort_test.cpp
@@ -1,67 +1,142 @@
 #include <deque>
 #include <vector>
+#include <utility>
 #include <gtest/gtest.h>
 #include <zaf/base/container/utility/sort.h>
 
-namespace internal {
+namespace {
 
-template<typename C>
-bool CheckElementOrder(const C& container) {
-	int index = 0;
-	for (int each_value : container) {
-		if (each_value != index) {
-			return false;
-		}
-		index++;
-	}
-	return true;
+using namespace std::rel_ops;
+
+class Object {
+public:
+    Object(int v, const std::string& t) : value(v), tag(t) { }
+
+    int value{};
+    std::string tag{};
+};
+
+bool operator<(const Object& o1, const Object& o2) {
+    return o1.value < o2.value;
+}
+
+bool operator==(const Object& o1, const Object& o2) {
+    return (o1.value == o2.value) && (o1.tag == o2.tag);
 }
 
 
 template<template<typename E, typename ...> class C>
 bool TestSort() {
 
-	C<int> container{ 4, 3, 2, 1, 0 };
+    C<Object> container{ { 4, "" }, { 3, "" }, { 2, "" }, { 1, "" }, { 0, "" } };
+    const C<Object> expected{ { 0, "" }, { 1, "" }, { 2, "" }, { 3, "" }, { 4, "" } };
 
 	auto new_container = zaf::MakeSorted(container);
-	if (!CheckElementOrder(new_container)) {
+	if (new_container != expected) {
 		return false;
 	}
 
 	zaf::Sort(container);
-	return CheckElementOrder(container);
+    return container == expected;
 }
 
 
 template<template<typename E, typename ...> class C>
 bool TestSortWithPredicate() {
 
-	C<int> container{ 4, 3, 2, 1, 0 };
+    C<Object> container{ { 0, "" }, { 1, "" }, { 2, "" }, { 3, "" }, { 4, "" } };
+    const C<Object> expected{ { 4, "" }, { 3, "" }, { 2, "" }, { 1, "" }, { 0, "" } };
 
-    auto new_container = zaf::MakeSorted(container, [](int v1, int v2) {
-        return v1 < v2;
-    });
-	if (!CheckElementOrder(new_container)) {
+    auto predicate = [](const Object& v1, const Object& v2) {
+        return v1 > v2;
+    };
+
+    auto new_container = zaf::MakeSorted(container, predicate);
+	if (new_container != expected) {
 		return false;
 	}
 
-	zaf::Sort(container, [](int v1, int v2) {
-		return v1 < v2;
-	});
-	return CheckElementOrder(container);
+	zaf::Sort(container, predicate);
+    return container == expected;
+}
+
+
+template<template<typename E, typename ...> class C>
+bool TestStableSort() {
+
+    C<Object> container{ 
+        { 0, "a" }, { 9, "b" }, { 9, "c" }, { 9, "d" }, { 2, "e" },
+        { 9, "f" }, { 3, "g" }, { 9, "h" }, { 4, "i" }, { 1, "j" },
+    };
+    const C<Object> expected{
+        { 0, "a" }, { 1, "j" }, { 2, "e" }, { 3, "g" }, { 4, "i" }, 
+        { 9, "b" }, { 9, "c" }, { 9, "d" }, { 9, "f" }, { 9, "h" },
+    };
+
+    auto new_container = zaf::MakeStableSorted(container);
+    if (new_container != expected) {
+        return false;
+    }
+
+    zaf::StableSort(container);
+    return container == expected;
+}
+
+
+template<template<typename E, typename ...> class C>
+bool TestStableSortWithPredicate() {
+
+    C<Object> container{
+        { 0, "a" }, { 9, "b" }, { 9, "c" }, { 9, "d" }, { 2, "e" },
+        { 9, "f" }, { 3, "g" }, { 9, "h" }, { 4, "i" }, { 1, "j" },
+    };
+    const C<Object> expected{
+        { 9, "b" }, { 9, "c" }, { 9, "d" }, { 9, "f" }, { 9, "h" },
+        { 4, "i" }, { 3, "g" }, { 2, "e" }, { 1, "j" }, { 0, "a" },
+    };
+
+    auto predicate = [](const Object& o1, const Object& o2) {
+        return o1.value > o2.value;
+    };
+
+    auto new_container = zaf::MakeStableSorted(container, predicate);
+    if (new_container != expected) {
+        return false;
+    }
+
+    zaf::StableSort(container, predicate);
+    return container == expected;
 }
 
 }
 
 TEST(Sort, Normal) {
 
-	ASSERT_TRUE(internal::TestSort<std::vector>());
-	ASSERT_TRUE(internal::TestSort<std::deque>());
+	ASSERT_TRUE(TestSort<std::vector>());
+	ASSERT_TRUE(TestSort<std::deque>());
+    ASSERT_TRUE(TestSort<std::list>());
 }
 
 
 TEST(Sort, Predicate) {
 
-	ASSERT_TRUE(internal::TestSortWithPredicate<std::vector>());
-	ASSERT_TRUE(internal::TestSortWithPredicate<std::deque>());
+	ASSERT_TRUE(TestSortWithPredicate<std::vector>());
+	ASSERT_TRUE(TestSortWithPredicate<std::deque>());
+    ASSERT_TRUE(TestSortWithPredicate<std::list>());
+}
+
+
+TEST(Sort, Stable) {
+
+    ASSERT_TRUE(TestStableSort<std::vector>());
+    ASSERT_TRUE(TestStableSort<std::deque>());
+    ASSERT_TRUE(TestStableSort<std::list>());
+}
+
+
+TEST(Sort, StableSortWithPredicate) {
+
+    ASSERT_TRUE(TestStableSortWithPredicate<std::vector>());
+    ASSERT_TRUE(TestStableSortWithPredicate<std::deque>());
+    ASSERT_TRUE(TestStableSortWithPredicate<std::list>());
 }
