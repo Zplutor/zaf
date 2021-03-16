6ac43e7ba58cc037669efc78716f460cc157b3e1
zplutor <zplutor@qq.com>
2021-03-17 01:24:57 +0800

    Adjust interface of Handle.

--- src/zaf/base/handle.h
+++ src/zaf/base/handle.h
@@ -6,30 +6,48 @@ namespace zaf {
 
 class Handle {
 public:
-    Handle() {}
+    Handle() = default;
     explicit Handle(HANDLE value) : value_(value) { }
 
-    ~Handle() {
-        if (value_ != nullptr && value_ != INVALID_HANDLE_VALUE) {
-            CloseHandle(value_);
-        }
-    }
+    Handle(const Handle&) = delete;
+    Handle& operator=(const Handle&) = delete;
 
     Handle(Handle&& other) : value_(other.value_) {
         other.value_ = nullptr;
     }
 
     Handle& operator=(Handle&& other) {
-        value_ = other.value_;
+        Reset(other.value_);
         other.value_ = nullptr;
         return *this;
     }
 
-    Handle(const Handle&) = delete;
-    Handle& operator=(const Handle&) = delete;
+    Handle& operator=(HANDLE new_value) {
+        Reset(new_value);
+        return *this;
+    }
+
+    ~Handle() {
+        Reset();
+    }
+
+    void Reset(HANDLE new_value = nullptr) {
+        if (IsValid()) {
+            CloseHandle(value_);
+        }
+        value_ = new_value;
+    }
+
+    bool IsValid() const {
+        return (value_ != nullptr) && (value_ != INVALID_HANDLE_VALUE);
+    }
 
     HANDLE GetValue() const {
         return value_;
+    }
+
+    explicit operator bool() const {
+        return IsValid();
     }
 
 private:
