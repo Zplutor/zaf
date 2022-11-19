df16dd7a75c554de5f3c54ee68582a0714b3c065
zplutor <zplutor@qq.com>
2022-11-19 21:38:02 +0800

    Sort properties of control.

--- src/zaf/control/property_grid/control_config.cpp
+++ src/zaf/control/property_grid/control_config.cpp
@@ -8,16 +8,37 @@ namespace zaf::property_grid {
 void ControlConfig::FilterProperties(PropertyTable& property_table) {
 
     auto list = property_table.GetList(Control::Type);
-    if (list) {
-        list->EraseByNames({
-            L"Parent",
-            L"Window",
-            L"X",
-            L"Y",
-            L"Position",
-            L"Size",
-        });
+    if (!list) {
+        return;
     }
+
+    list->EraseByNames({
+        L"Parent",
+        L"Window",
+        L"X",
+        L"Y",
+        L"Position",
+        L"Size",
+        L"Width",
+        L"Height",
+        L"ContentSize",
+    });
+
+    list->SortByNames({
+        L"Name",
+        L"AbsoluteRect",
+        L"Rect",
+        L"Border",
+        L"Padding",
+        L"Margin",
+        L"ContentRect",
+        L"MinWidth",
+        L"MaxWidth",
+        L"MinHeight",
+        L"MaxHeight",
+        L"AutoWidth",
+        L"AutoHeight",
+    });
 }
 
 }
