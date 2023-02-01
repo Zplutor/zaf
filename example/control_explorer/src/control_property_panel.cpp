4e8e06a872856b846f36ac96a4b23b1bf3ab3088
zplutor <zplutor@qq.com>
2023-02-01 12:33:27 +0800

    Add refresh button.

--- example/control_explorer/src/control_property_panel.cpp
+++ example/control_explorer/src/control_property_panel.cpp
@@ -8,15 +8,26 @@ void ControlPropertyPanel::Initialize() {
 
     SetLayouter(zaf::Create<zaf::VerticalLayouter>());
 
+    refresh_button_ = zaf::Create<zaf::Button>();
+    refresh_button_->SetText(L"Refresh");
+    refresh_button_->SetFixedHeight(30);
+    refresh_button_->SetIsVisible(false);
+    Subscriptions() += refresh_button_->ClickEvent().Subscribe(std::bind([this]() {
+        property_grid_->RefreshValues();
+    }));
+    AddChild(refresh_button_);
+
     property_grid_ = zaf::Create<zaf::PropertyGrid>();
     property_grid_->SetBorder(zaf::Frame{});
     property_grid_->SetAutoHideScrollBars(true);
-
+    property_grid_->SetIsVisible(false);
     AddChild(property_grid_);
 }
 
 
 void ControlPropertyPanel::SetExploredControl(const std::shared_ptr<zaf::Control>& control) {
 
+    refresh_button_->SetIsVisible(true);
+    property_grid_->SetIsVisible(true);
     property_grid_->SetTargetObject(control);
 }
--- example/control_explorer/src/control_property_panel.h
+++ example/control_explorer/src/control_property_panel.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <zaf/control/button.h>
 #include <zaf/control/property_grid.h>
 
 class ControlPropertyPanel : public zaf::Control {
@@ -9,5 +10,6 @@ public:
     void SetExploredControl(const std::shared_ptr<zaf::Control>& control);
 
 private:
+    std::shared_ptr<zaf::Button> refresh_button_;
     std::shared_ptr<zaf::PropertyGrid> property_grid_;
 };
