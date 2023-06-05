44419da2d0460e62172403e7e6d9aa2a91bb2a82
zplutor <zplutor@qq.com>
2023-06-06 00:29:17 +0800

    Unitest for DropFilesData.

--- unittest/case/clipboard/drop_files_data_test.cpp
+++ unittest/case/clipboard/drop_files_data_test.cpp
@@ -1,6 +1,9 @@
 #include <gtest/gtest.h>
+#include <zaf/base/error/basic_error.h>
 #include <zaf/clipboard/drop_files_data.h>
+#include "utility/assert.h"
 
+using namespace zaf;
 using namespace zaf::clipboard;
 
 TEST(DropFilesDataTest, MediumTransfer) {
@@ -21,4 +24,51 @@ TEST(DropFilesDataTest, MediumTransfer) {
         L"C:\\Windows\\Notepad.exe",
         L"C:\\Program Files\\RunAnywhere.exe"
     }));
+}
+
+
+TEST(DropFilesDataTest, SaveToMediumWithInvalidValue) {
+
+    //Empty files.
+    {
+        DropFilesData data;
+        ASSERT_THROW_ERRC(
+            data.SaveToMedium(Format(FormatType::DropFiles)), 
+            BasicErrc::InvalidValue);
+    }
+
+    //Not rooted path.
+    {
+        DropFilesData data{ { L"Windows\\notepad.exe" }};
+        ASSERT_THROW_ERRC(
+            data.SaveToMedium(Format(FormatType::DropFiles)),
+            BasicErrc::InvalidValue);
+    }
+}
+
+
+TEST(DropFilesDataTest, SaveToMediumWithUnsupportedArguments) {
+
+    DropFilesData data{ { L"C:\\Windows\\Notepad.exe" } };
+
+    ASSERT_THROW_ERRC(data.SaveToMedium(Format(FormatType::Text)), BasicErrc::Unsupported);
+    ASSERT_THROW_ERRC(
+        data.SaveToMedium(Format(FormatType::DropFiles, MediumType::File)), 
+        BasicErrc::Unsupported);
+}
+
+
+TEST(DropFilesDataTest, LoadFromMediumWithUnsupportedArguments) {
+
+    auto global_mem = GlobalMem::Alloc(10, GlobalMemFlags::Movable);
+    auto medium = Medium::FromGlobalMem(std::move(global_mem));
+
+    DropFilesData data;
+    ASSERT_THROW_ERRC(
+        data.LoadFromMedium(Format(FormatType::Text), medium),
+        BasicErrc::Unsupported);
+
+    ASSERT_THROW_ERRC(
+        data.LoadFromMedium(Format(FormatType::DropFiles, MediumType::GlobalMem), {}),
+        BasicErrc::Unsupported);
 }
