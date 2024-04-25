ed57fdfe835fb9a53a5f8477cd76fd5b65c9072a
zplutor <zplutor@qq.com>
2024-04-25 12:55:17 +0800

    Remove no used code.

--- src/zaf/internal/textual/text_box_ime_manager.cpp
+++ src/zaf/internal/textual/text_box_ime_manager.cpp
@@ -26,39 +26,9 @@ void TextBoxIMEManager::HandleIMEComposition(const IMECompositionInfo& event_inf
 
     auto text_layout = Context().GetTextLayout();
 
-    COMPtr<IDWriteFontCollection> font_collection;
-    HRESULT hresult = text_layout.Inner()->GetFontCollection(font_collection.Reset());
-    ZAF_THROW_IF_COM_ERROR(hresult);
-
-    auto font_family_name = text_layout.GetFontFamilyName(0, nullptr);
-    UINT font_index{};
-    BOOL exist{};
-    hresult = font_collection->FindFamilyName(font_family_name.c_str(), &font_index, &exist);
-    ZAF_THROW_IF_COM_ERROR(hresult);
-
-    COMPtr<IDWriteFontFamily> font_family;
-    hresult = font_collection->GetFontFamily(font_index, font_family.Reset());
-    ZAF_THROW_IF_COM_ERROR(hresult);
-
-    COMPtr<IDWriteFont> dwrite_font;
-    hresult = font_family->GetFont(0, dwrite_font.Reset());
-    ZAF_THROW_IF_COM_ERROR(hresult);
-
-    DWRITE_FONT_METRICS font_metrics{};
-    dwrite_font->GetMetrics(&font_metrics);
-
-    auto font_size = Context().Owner().FontSize();
-
-    float ascent = (font_metrics.ascent * font_size) / (font_metrics.designUnitsPerEm);
-    float descent = (font_metrics.descent * font_size) / (font_metrics.designUnitsPerEm);
-    float cap_height = (font_metrics.capHeight * font_size) / (font_metrics.designUnitsPerEm);
-    float line_gap = (font_metrics.lineGap * font_size) / (font_metrics.designUnitsPerEm);
-
     auto context = InputMethodContext::FromWindowHandle(event_info.Message().WindowHandle());
-    auto caret_rect = Context().CaretManager().GetCaretRect();
+    auto caret_rect = Context().CaretManager().GetCaretRectInContent();
     caret_rect.AddOffset(Context().Owner().ContentRectInWindow()->position);
-
-    //caret_rect.position.y -= ascent - cap_height;
 
     context.MoveCompositionWindow(caret_rect.position);
 }
