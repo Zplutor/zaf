e14dbba4d62c0ddb5b41009b5aa31632293ea7fb
zplutor <zplutor@qq.com>
2024-04-29 12:51:34 +0800

    Add more overloads for Observer::Create.

--- src/zaf/rx/observer.h
+++ src/zaf/rx/observer.h
@@ -8,14 +8,23 @@ namespace zaf {
 template<typename T>
 class Observer {
 public:
-    static Observer Create(OnNext<T> on_next, OnCompleted on_completed = nullptr) {
+    static Observer Create() {
+        return Create(nullptr, nullptr, nullptr);
+    }
+
+    static Observer Create(OnNext<T> on_next) {
+        return Create(std::move(on_next), nullptr, nullptr);
+    }
+
+    static Observer Create(OnNext<T> on_next, OnError on_error) {
+        return Create(std::move(on_next), std::move(on_error), nullptr);
+    }
+
+    static Observer Create(OnNext<T> on_next, OnCompleted on_completed) {
         return Create(std::move(on_next), nullptr, std::move(on_completed));
     }
 
-    static Observer Create(
-        OnNext<T> on_next, 
-        OnError on_error = nullptr, 
-        OnCompleted on_completed = nullptr) {
+    static Observer Create(OnNext<T> on_next, OnError on_error, OnCompleted on_completed) {
 
         auto bridged_on_next = [on_next](const std::any& value) {
             if (on_next) {
--- unittest/case/rx/observer_test.cpp
+++ unittest/case/rx/observer_test.cpp
@@ -3,6 +3,129 @@
 
 using namespace zaf;
 
+//Make sure the five overloads work correctly.
+TEST(RxObserverTest, Create) {
+
+    //No arguments.
+    {
+        auto observer = Observer<int>::Create();
+        ASSERT_NO_THROW(observer.OnNext(1));
+        ASSERT_NO_THROW(observer.OnCompleted());
+    }
+
+    {
+        auto observer = Observer<int>::Create();
+        ASSERT_NO_THROW(observer.OnError(std::logic_error("")));
+    }
+
+    //OnNext only.
+    {
+        int on_next_value{};
+        auto observer = Observer<int>::Create([&](int value) {
+            on_next_value = value;
+        });
+        observer.OnNext(1);
+        ASSERT_EQ(on_next_value, 1);
+        ASSERT_NO_THROW(observer.OnCompleted());
+    }
+
+    {
+        auto observer = Observer<int>::Create([](int) {});
+        ASSERT_NO_THROW(observer.OnError(std::logic_error("")));
+    }
+
+    //OnNext, OnError.
+    {
+        int on_next_value{};
+        double on_error_value{};
+        auto observer = Observer<int>::Create(
+            [&](int value) {
+                on_next_value = value;
+            },
+            [&](const std::exception_ptr& error) {
+                try {
+                    std::rethrow_exception(error);
+                }
+                catch (double double_value) {
+                    on_error_value = double_value;
+                }
+            });
+
+        observer.OnNext(2);
+        ASSERT_EQ(on_next_value, 2);
+        observer.OnError(double(2.2));
+        ASSERT_EQ(on_error_value, double(2.2));
+    }
+
+    {
+        auto observer = Observer<int>::Create([](int) {}, [](const std::exception_ptr&) {});
+        ASSERT_NO_THROW(observer.OnCompleted());
+    }
+
+    //OnNext, OnCompleted.
+    {
+        int on_next_value{};
+        bool on_completed{};
+        auto observer = Observer<int>::Create(
+            [&](int value) {
+                on_next_value = value;
+            },
+            [&]() {
+                on_completed = true;
+            });
+
+        observer.OnNext(3);
+        ASSERT_EQ(on_next_value, 3);
+
+        observer.OnCompleted();
+        ASSERT_TRUE(on_completed);
+    }
+
+    {
+        auto observer = Observer<int>::Create([](int) {}, []() {});
+        ASSERT_NO_THROW(observer.OnError(float(3.14)));
+    }
+
+    //OnNext, OnError, OnCompleted
+    {
+        int on_next_value{};
+        double on_error_value{};
+        auto observer = Observer<int>::Create(
+            [&](int value) {
+                on_next_value = value;
+            },
+            [&](const std::exception_ptr& error) {
+                try {
+                    std::rethrow_exception(error);
+                }
+                catch (double double_value) {
+                    on_error_value = double_value;
+                }
+            },
+            [&]() { });
+
+        observer.OnNext(4);
+        ASSERT_EQ(on_next_value, 4);
+
+        observer.OnError(double(4.4));
+        ASSERT_EQ(on_error_value, double(4.4));
+    }
+
+    {
+        bool on_completed{};
+        auto observer = Observer<int>::Create(
+            [](int value) {},
+            [](const std::exception_ptr&) { },
+            [&]() {
+                on_completed = true;
+            });
+
+        observer.OnCompleted();
+        ASSERT_TRUE(on_completed);
+    }
+}
+
+
 TEST(RxObserverTest, OnError) {
 
     std::string string_error;
