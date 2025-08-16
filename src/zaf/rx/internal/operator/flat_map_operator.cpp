0dc7659a96797d4f38e9fae5f27b8e31b41a8201
zplutor <zplutor@qq.com>
2025-08-16 20:15:10 +0800

    Fix thread race condition of FlatMapOperator.

--- src/zaf/rx/internal/operator/flat_map_operator.cpp
+++ src/zaf/rx/internal/operator/flat_map_operator.cpp
@@ -1,4 +1,5 @@
 #include <zaf/rx/internal/operator/flat_mapper.h>
+#include <optional>
 #include <zaf/base/as.h>
 #include <zaf/base/container/utility/contain.h>
 #include <zaf/base/container/utility/map.h>
@@ -18,6 +19,7 @@ public:
         Producer(std::move(next_observer)),
         mapper_(std::move(mapper)) {
 
+        emitting_mapper_subs_.emplace();
     }
 
     ~FlatMapProducer() {
@@ -25,7 +27,7 @@ public:
     }
 
     void Run(const std::shared_ptr<ObservableCore>& source) {
-        source_subscription_ = source->Subscribe(
+        source_sub_ = source->Subscribe(
             ObserverShim::FromWeak(As<ObserverCore>(shared_from_this())));
     }
 
@@ -36,45 +38,61 @@ public:
             mapped_observable = mapper_(value);
         }
         catch (...) {
-            if (source_subscription_) {
-                source_subscription_->Dispose();
-            }
-            TryToDeliverOnError(std::current_exception());
+            EmitOnError(std::current_exception());
             return;
         }
 
+        std::weak_ptr<FlatMapProducer> weak_this = As<FlatMapProducer>(shared_from_this());
+
         auto sub_id = ++mapper_subs_count_;
         auto mapper_sub = mapped_observable->Subscribe(ObserverShim::MakeShared(
-            [this](const std::any& value) {
-                if (!IsDisposed()) {
-                    EmitOnNext(value);
+            [weak_this](const std::any& value) {
+                if (auto strong_this = weak_this.lock()) {
+                    strong_this->EmitOnNext(value);
                 }
             },
-            [this, sub_id](const std::exception_ptr& error) {
-                // Observer will be destroyed once the subscription is disposed, hence we keep 
-                // the subscription alive until we have done everything.
-                auto sub = OnMapperSubFinished(sub_id);
-                TryToDeliverOnError(error);
+            [weak_this, sub_id](const std::exception_ptr& error) {
+                if (auto strong_this = weak_this.lock()) {
+                    strong_this->OnMapperTerminated(sub_id);
+                    strong_this->EmitOnError(error);
+                }
             },
-            [this, sub_id]() {
-                //Same as above.
-                auto sub = OnMapperSubFinished(sub_id);
-                TryToDeliverOnCompleted();
+            [weak_this, sub_id]() {
+                if (auto strong_this = weak_this.lock()) {
+                    strong_this->OnMapperTerminated(sub_id);
+                    strong_this->TryEmitOnCompleted();
+                }
             }
         ));
-
-        if (!Contain(finished_mapper_subs_, sub_id)) {
-            emitting_mapper_subs_[sub_id] = mapper_sub;
+        
+        if (mapper_sub) {
+            bool should_dispose{};
+            {
+                std::lock_guard<std::mutex> lock(mutex_);
+                if (emitting_mapper_subs_) {
+                    if (!Contain(terminated_mapper_subs_, sub_id)) {
+                        (*emitting_mapper_subs_)[sub_id] = std::move(mapper_sub);
+                    }
+                }
+                else {
+                    // If the producer is disposed, mapper subscription should be disposed 
+                    // immediately.
+                    should_dispose = true;
+                }
+            }
+            if (should_dispose) {
+                mapper_sub->Dispose();
+            }
         }
     }
 
     void OnError(const std::exception_ptr& error) override {
-        TryToDeliverOnError(error);
+        EmitOnError(error);
     }
 
     void OnCompleted() override {
-        is_source_finished_ = true;
-        TryToDeliverOnCompleted();
+        is_source_completed_ = true;
+        TryEmitOnCompleted();
     }
 
     void OnDispose() noexcept override {
@@ -82,56 +100,59 @@ public:
     }
 
 private:
-    std::shared_ptr<SubscriptionCore> OnMapperSubFinished(std::size_t sub_id) {
+    void OnMapperTerminated(std::size_t sub_id) {
 
-        std::shared_ptr<SubscriptionCore> sub;
-        auto iterator = emitting_mapper_subs_.find(sub_id);
-        if (iterator != emitting_mapper_subs_.end()) {
-
-            sub = iterator->second;
-            emitting_mapper_subs_.erase(iterator);
-        }
-        
-        finished_mapper_subs_.insert(sub_id);
-        return sub;
-    }
-
-    void TryToDeliverOnError(const std::exception_ptr& error) {
-        if (!IsDisposed()) {
-            EmitOnError(error);
+        std::lock_guard<std::mutex> lock(mutex_);
+        terminated_mapper_subs_.insert(sub_id);
+        if (emitting_mapper_subs_) {
+            emitting_mapper_subs_->erase(sub_id);
         }
     }
 
-    void TryToDeliverOnCompleted() {
+    void TryEmitOnCompleted() {
 
-        if (IsDisposed()) {
-            return;
+        bool is_totally_terminated{};
+        {
+            std::lock_guard<std::mutex> lock(mutex_);
+            if (is_source_completed_ && (terminated_mapper_subs_.size() == mapper_subs_count_)) {
+                is_totally_terminated = true;
+            }
         }
 
-        if (is_source_finished_ && finished_mapper_subs_.size() == mapper_subs_count_) {
+        if (is_totally_terminated) {
             EmitOnCompleted();
         }
     }
 
     void DoDisposal() noexcept {
 
-        if (source_subscription_) {
-            source_subscription_->Dispose();
-            source_subscription_.reset();
+        if (source_sub_) {
+            source_sub_->Dispose();
+            source_sub_.reset();
         }
 
-        emitting_mapper_subs_.clear();
+        std::map<std::size_t, std::shared_ptr<SubscriptionCore>> mapper_subs;
+        {
+            std::lock_guard<std::mutex> lock(mutex_);
+            mapper_subs = std::move(*emitting_mapper_subs_);
+        }
+        for (const auto& pair : mapper_subs) {
+            pair.second->Dispose();
+        }
+
         mapper_ = nullptr;
     }
 
 private:
-    std::shared_ptr<SubscriptionCore> source_subscription_;
+    std::shared_ptr<SubscriptionCore> source_sub_;
     FlatMapper mapper_;
-    std::size_t mapper_subs_count_{};
 
-    bool is_source_finished_{};
-    std::map<std::size_t, std::shared_ptr<SubscriptionCore>> emitting_mapper_subs_;
-    std::set<std::size_t> finished_mapper_subs_;
+    std::mutex mutex_;
+    std::atomic<bool> is_source_completed_{};
+    std::atomic<std::size_t> mapper_subs_count_{};
+    // Will be std::nullopt if disposed.
+    std::optional<std::map<std::size_t, std::shared_ptr<SubscriptionCore>>> emitting_mapper_subs_;
+    std::set<std::size_t> terminated_mapper_subs_;
 };
 
 }
